22bc1fed14da8e3f219bc06bef91d3d807cd9a43
Glenn Goodrich <glenn.goodrich@method.com>
2024-04-23 15:08:32 +0200

    Initial Commit

---
+++ Piazza/PiazzaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PiazzaApp.swift
+//  Piazza
+//
+//  Created by Glenn Goodrich on 23/4/24.
+//
+
+import SwiftUI
+
+@main
+struct PiazzaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
